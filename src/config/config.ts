5110683fffa212ac4a5ec6a5689da4b6a7cb9d86
Oswaldo Ferreira <oswluizf@gmail.com>
2020-10-22 18:30:33 -0300

    Add Dockerfile

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,7 +9,7 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': `${process.env.URL}:${process.env.PORT}`,
+  'url': process.env.URL,
   'port': process.env.PORT,
   'jwt': {
     'secret': process.env.JWT_SECRET,
